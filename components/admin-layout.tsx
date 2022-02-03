b0750804ebb9f470c981973c5cc629250e5eee3c
Steve Suranant <053steve@gmail.com>
2022-02-03 23:18:54 +0700

    [CODE] multiple layouts implementation

---
+++ components/admin-layout.tsx
@@ -0,0 +1,8 @@
+export default function AdminLayout({children}) {
+  return (
+    <div className="w-full min-h-screen bg-gray-50 flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+      admin layout
+      {children}
+    </div>
+  )
+}
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,22 @@
 import type { NextPage } from 'next'
+import AdminLayout from '../components/admin-layout';
 
-const Dashboard: NextPage = () => {
+const Dashboard = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
             Dashboard
         </h1>
     </div>
+  )
+}
+
+Dashboard.getLayout = function getLayout(page) {
+  return (
+    <AdminLayout>
+      {page}
+    </AdminLayout>
+
   )
 }
 
